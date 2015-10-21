df108c26e05a209c6aced21e9e8baf37d3f53c08
sundy-li <543950155@qq.com>
2015-10-21 14:15:13 +0800

    fix:build jar script

--- build.sh
+++ build.sh
@@ -20,10 +20,10 @@ if [[ ! -d "target" ]]; then
 fi
 cd target
 
-BUILD_JAR=dcmonitor-1.0.one-jar.jar
+BUILD_JAR=dcmonitor-*.one-jar.jar
 jar -xf ${BUILD_JAR}
 rm -rf META-INF OneJar.class doc src com .version ${BUILD_JAR} classes maven-archiver surefire classes generated-sources
-mv main/dcmonitor-1.0.jar ./dcmonitor-1.0.jar
+mv main/dcmonitor-*.jar ./
 rm -rf dependencies
 mv -f lib dependencies
 rm -rf main
