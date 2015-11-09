9592b148822d82c85c11b4679917a45137ad019f
sundy-li <543950155@qq.com>
2015-11-09 11:45:00 +0800

    update run.sh version

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ nohup java \
 -server \
 -XX:+PrintGCDetails \
 -XX:+PrintGCTimeStamps \
--classpath config:target/dependencies/*:target/dcmonitor-1.0.jar \
+-classpath config:target/dependencies/*:target/dcmonitor-0.1.4.jar \
 com.sf.monitor.DCMonitor > /data/logs/dcmonitor/dcmonitor.run.out 2>&1 &
